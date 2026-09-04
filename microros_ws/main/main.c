e65e1cb5812db1ed0ac69886f4443a4e3a83eb5a
Ignacio Facello <ignacio.facello@mi.unc.edu.ar>
2026-09-04 01:33:08 -0300

    working version

--- microros_ws/main/main.c
+++ microros_ws/main/main.c
@@ -71,54 +71,37 @@ std_msgs__msg__Int32 encoder_msg;
 
 static encoder_handle_t encoder_h;
 
-static pot_handle_t potentiometer_h;
-static pot_config_t poten_config = {
+static pot_handle_t pot_handle;
+static pot_config_t pot_cfg = {
     .unit = ADC_UNIT_1,
     .channel = ADC_CHANNEL_7, // GPIO35
     .bitwidth = ADC_BITWIDTH_12,
     .atten = ADC_ATTEN_DB_6,
     .max_voltage = 2.2f,
-    .avg_samples = 10,
+    .avg_samples = 25,
 };
 
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
     float poten_percent = 0.0f;
-    int32_t encoder_count = 0.0f;
+    int32_t encoder_count = 0;
 
-    pot_update(potentiometer_h);
+    if (pot_handle != NULL)
+    {
+        pot_update(pot_handle);
+        pot_get_percentage(pot_handle, &poten_percent);
+        poten_msg.data = poten_percent;
+        RCSOFTCHECK(rcl_publish(&poten_publisher, &poten_msg, NULL));
+    }
 
-    as5600_get_angle(encoder_h, (uint16_t *)&encoder_count);
-    encoder_msg.data = encoder_count;
-    RCSOFTCHECK(rcl_publish(&encoder_publisher, &encoder_msg, NULL));
+    if (encoder_h != NULL) {
+        as5600_get_angle(encoder_h, (uint16_t *)&encoder_count);
+        encoder_msg.data = encoder_count;
+        RCSOFTCHECK(rcl_publish(&encoder_publisher, &encoder_msg, NULL));
+    }
 
-    pot_get_percentage(potentiometer_h, &poten_percent);
-    poten_msg.data = poten_percent;
-    RCSOFTCHECK(rcl_publish(&poten_publisher, &poten_msg, NULL));
-}
-
-esp_err_t init_publishers(rcl_node_t *node)
-{
-    rcl_ret_t rc;
-
-    // Inicialización del publicador de posición
-    rc = rclc_publisher_init_default(
-        &poten_publisher,
-        node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-        "potentiometer"
-    );
-
-    if (rc != RCL_RET_OK) { ESP_LOGE(TAG, "Failed to init potentiometer publisher: %d", rc); }
-    rc = rclc_publisher_init_default(
-        &encoder_publisher,
-        node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "encoder"
-    );
-    if (rc != RCL_RET_OK) { ESP_LOGE(TAG, "Failed to init encoder publisher: %d", rc); }
-    return rc;
+    ESP_LOGI(TAG, "Potentiometer: %.2f%%, Encoder: %d", poten_percent, encoder_count);
 }
 
 /* ── Tarea micro-ROS ────────────────────────────────────────── */
@@ -208,9 +191,32 @@ void micro_ros_task(void *arg)
     }
     ESP_LOGI(TAG, "Node created successfully");
 
-    // Inicialización del publicador
-    rc = init_publishers(&node);
-    if (rc != RCL_RET_OK) { vTaskDelete(NULL); return; }
+    // Inicialización del publicador de posición
+    rc = rclc_publisher_init_default(
+        &poten_publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+        "potentiometer"
+    );
+
+    if (rc != RCL_RET_OK) {
+        ESP_LOGE(TAG, "Failed to init potentiometer publisher: %d", rc);
+        vTaskDelete(NULL);
+        return;
+    }
+    else { ESP_LOGI(TAG, "Potentiometer publisher initialized"); }
+
+    rc = rclc_publisher_init_default(
+        &encoder_publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "encoder"
+    );
+    if (rc != RCL_RET_OK) {
+        ESP_LOGE(TAG, "Failed to init encoder publisher: %d", rc);
+        vTaskDelete(NULL);
+        return; }
+    else { ESP_LOGI(TAG, "Encoder publisher initialized"); }
 
     // Inicialización del Timer
     rcl_timer_t timer = rcl_get_zero_initialized_timer();
@@ -260,11 +266,17 @@ void micro_ros_task(void *arg)
     }
     ESP_LOGI(TAG, "Executor configured successfully. Starting main loop...");
 
+    pot_init(&pot_cfg, &pot_handle);
+    as5600_init(&encoder_h);
+
     while (1)
     {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
         usleep(10000);
     }
+
+    pot_deinit(pot_handle);
+    as5600_deinit(encoder_h);
 
     RCCHECK(rcl_publisher_fini(&poten_publisher, &node));
     RCCHECK(rcl_node_fini(&node));
@@ -277,12 +289,7 @@ void app_main(void)
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
 
-    pot_init(&poten_config, &potentiometer_h);
-    as5600_init(&encoder_h);
-
     xTaskCreate(micro_ros_task, "micro_ros_task",
                 MICRO_ROS_APP_STACK, NULL, MICRO_ROS_APP_TASK_PRIO, NULL);
 
-    pot_deinit(potentiometer_h);
-    as5600_deinit(encoder_h);
 }
