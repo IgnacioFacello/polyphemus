b2f1d99984b44c0a01ee8eaec1e14593e076d956
Ignacio Facello <ignacio.facello@mi.unc.edu.ar>
2026-09-05 10:14:28 -0300

    feat: print raw and processed count over uart

--- microros_ws/main/main.c
+++ microros_ws/main/main.c
@@ -87,7 +87,8 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
     float poten_percent = 0.0f;
     float poten_rad = 0.0f;
-    int32_t encoder_count = 0;
+    uint16_t encoder_count = 0;
+    uint16_t encoder_raw = 0;
     float encoder_rad = 0.0f;
 
     uint8_t enc_status = 0x00;
@@ -104,7 +105,9 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
     if (encoder_h != NULL) {
         as5600_get_status(encoder_h, &enc_status);
         if (enc_status & 0x20) {
-            as5600_get_angle(encoder_h, (uint16_t *)&encoder_count);
+            as5600_get_angle(encoder_h, &encoder_count);
+            as5600_get_raw_angle(encoder_h, &encoder_raw);
+            ESP_LOGI(TAG, "Encoder count: %d, raw: %d", encoder_count, encoder_raw);
             encoder_rad = ((float)encoder_count/4095) * 2*PI;
             encoder_msg.data = encoder_rad;
             RCSOFTCHECK(rcl_publish(&encoder_publisher, &encoder_msg, NULL));
@@ -113,7 +116,7 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
         }
     }
 
-    ESP_LOGI(TAG, "Potentiometer: %.2f, Encoder: %.2f", poten_rad, encoder_rad);
+    //ESP_LOGI(TAG, "Potentiometer: %.2f, Encoder: %.2f", poten_rad, encoder_rad);
 }
 
 /* ── Tarea micro-ROS ────────────────────────────────────────── */
