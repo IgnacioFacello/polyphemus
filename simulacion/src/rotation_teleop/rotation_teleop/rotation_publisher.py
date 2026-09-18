a0aeac7f8773aead93125f0af4d2a71ed00a7449
Micaela Grande <micaela.grande@mi.unc.edu.ar>
2026-09-18 00:27:57 -0300

    Correcion de formato de impresion

--- simulacion/src/rotation_teleop/rotation_teleop/rotation_publisher.py
+++ simulacion/src/rotation_teleop/rotation_teleop/rotation_publisher.py
@@ -10,30 +10,29 @@ class RotationPublisher(Node):
             Float32MultiArray, 'rotation_input', 10)
         self.subscriber = self.create_subscription(
             Float32MultiArray, 'vector_rotado', self.vector_rotado_callback, 10)
-        
+
+
     def vector_rotado_callback(self, msg):
-        vector = msg.data
-        vector_exacto = vector[0:3]
-        vector_micro = vector[3:6]
-        
-        print(f"Vector de una sola rotacion:{vector_exacto}\n"
-              f"vector con micro-rotaciones: {vector_micro}")
+        vector_exacto = list(msg.data[0:3])
+        vector_micro = list(msg.data[3:6])
+
+        print(f"Vector de una sola rotacion: {vector_exacto}\n"
+            f"vector con micro-rotaciones: {vector_micro}")
 
     def run(self):
 
         vector = list(map(float, input(
             "Ingresa las 3 componentes del vector (x,y,z) separados por espacio: ").split())
         )
-        print(f"Valores ingresados: {vector[0]}, {vector[1]}, {vector[2]}")
         angulos = list(map(float, input(
             "Ingresa tres angulos separados por espacio: ").split())
-)
-        print(f"Valores ingresados: {angulos[0]},  {angulos[1]},  {angulos[2]}")
+        )
+       
 
         paso = float(input("Ingrese la cantidad de paso:"))
-        print("La cantidad de paso es:", paso)
+        
 
-        msg =Float32MultiArray()
+        msg = Float32MultiArray()
         msg.data = vector + angulos + [paso]
 
         self.publisher_.publish(msg)
